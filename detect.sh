915ed7a719912bd721f2a6abde909f917ed78c24
VittalAB <vittal.a.badami0107@gmail.com>
2023-02-07 10:42:44 +0530

    updated model

--- detect.sh
+++ detect.sh
@@ -1,1 +1,1 @@
-python detect.py --weights best.pt --conf 0.5 --img-size 416 --source test/ --no-trace
+python detect.py --weights fire_best.pt --conf 0.65 --img-size 416 --source inference/tsw1.mp4 --no-trace
